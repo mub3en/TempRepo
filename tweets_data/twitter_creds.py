ed840ce6e1c5a31ecc2a2b9704886b4b8de70d37
Mubeen <hafizalimubeen@gmail.com>
2023-06-12 00:41:16 -0400

    Create twitter_creds.py

---
+++ tweets_data/twitter_creds.py
@@ -0,0 +1,7 @@
+#variables that contains the user credential information
+
+###### Twitter Account ########
+CONSUMER_KEY = "itDoesNotWork"
+CONSUMER_SECRET_KEY="getYourOwnKeys"
+ACCESS_TOKEN="getYourOwnTokens"
+ACCESS_TOKEN_SECRET="getYourOwnTokenSecret"
